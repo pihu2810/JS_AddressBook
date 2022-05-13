5f8686c39f4bb4e0961183f9e8b41d39b46e97f7
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-13 16:25:55 +0530

    Refactor to Sort Entries by alphabetically

--- UserINputOutPut.js
+++ UserINputOutPut.js
@@ -5,11 +5,13 @@ class UserInputOutput {
         console.log("Press 1 - To insert new contact");
         console.log("Press 2 - To display details");
         console.log("Press 3 - To edit contact details");
-        console.log("Press 4 - To delete contact")
-        console.log("Press 5 - To get count of contacts in address book")
-        console.log("Press 6 - To search person by city or state")
-        console.log("Press 7 - To exit");
+        console.log("Press 4 - To delete contact");
+        console.log("Press 5 - To get count of contacts in address book");
+        console.log("Press 6 - To search person by city or state");
+        console.log("Press 7 - To sort Contact List")
+        console.log("Press 8 - To exit");
         console.log();
         return parseInt(prompt("Enter your choice : "));
     }
 }
+module.exports = UserInputOutput;
